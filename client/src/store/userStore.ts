82af25c185de40a74c73c209a6dc605dc11d0ced
Stanley Claudius <stanley.claudius@binus.ac.id>
2023-12-26 14:25:04 +0700

    Integrate login functionality at frontend

--- client/src/store/userStore.ts
+++ client/src/store/userStore.ts
@@ -1,4 +1,12 @@
-const userState = {
+import { postDataAPI } from "../utils/fetchData"
+import { GlobalStoreState, IUserState } from "../utils/interface"
+
+interface ILoginData {
+  email: string
+  password: string
+}
+
+const userState: IUserState = {
   data: {},
   loading: true
 }
@@ -6,9 +14,32 @@ const userState = {
 const userStore = (set: any) => {
   return {
     userState,
-    // login: async(data: object) => {
-      
-    // }
+    login: async(data: ILoginData) => {
+      set((state: GlobalStoreState) => {
+        state.userState.loading = true
+      }, false, 'login/loading')
+
+      try {
+        const res = await postDataAPI('/user/login', data)
+
+        set((state: GlobalStoreState) => {
+          state.userState.data = {
+            user: res.data.user,
+            accessToken: res.data.accessToken
+          }
+          state.userState.loading = false
+          state.alertState.message = res.data.msg
+          state.alertState.type = 'success'
+        }, false, 'login/success')
+      } catch (err: any) {
+        set((state: GlobalStoreState) => {
+          state.userState.loading = false
+          state.userState.data = {}
+          state.alertState.message = err.response.data.msg
+          state.alertState.type = 'error'
+        }, false, 'login/error')
+      }
+    }
   }
 }
 
