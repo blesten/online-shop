aaa0872be65a8c293041bb7b93dee2014fb0696e
Stanley Claudius <stanley.claudius@binus.ac.id>
2024-01-13 16:10:36 +0700

    Create checkout page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import useStore from './store/store'
 import Dashboard from './pages/admin/Dashboard'
 import OwnerPick from './pages/admin/OwnerPick'
 import BottomAlert from './components/general/BottomAlert'
+import Checkout from './pages/Checkout'
 
 const App = () => {
   const { userState, refreshToken, readCart, readWishlist } = useStore()
@@ -39,6 +40,7 @@ const App = () => {
           <Route path='/register' element={<SignUp />} />
           <Route path='/products' element={<Products />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Checkout />} />
           <Route path='/admin' element={<Dashboard />} />
           <Route path='/admin/product' element={<Product />} />
           <Route path='/admin/category' element={<Category />} />
---
+++ client/src/config/key.example.ts
@@ -0,0 +1,2 @@
+export const LOCATION_API_KEY = 'YOUR_https://goapi.io/api-wilayah-indonesia_API_KEY'
+export const BITESHIP_AUTHORIZATION_KEY = 'YOUR_https://biteship.com_AUTHORIZATION_KEY'
