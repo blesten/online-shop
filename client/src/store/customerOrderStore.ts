61ac4a20d3ef6b60ffec559802a158faf2e62c7d
Stanley Claudius <stanley.claudius@binus.ac.id>
2024-01-15 22:06:25 +0700

    Create getCustomerOrder functionality

---
+++ client/src/store/customerOrderStore.ts
@@ -0,0 +1,42 @@
+import { getDataAPI } from "../utils/fetchData";
+import { GlobalStoreState, ICheckoutState } from "../utils/interface";
+
+const customerOrderState: ICheckoutState = {
+  data: [],
+  loading: false,
+  totalPage: 0,
+  totalData: 0
+}
+
+const customerOrderStore = (set: any) => {
+  return {
+    customerOrderState,
+    readCustomerOrder: async(token: string, page: number, limit: number, status: string) => {
+      set((state: GlobalStoreState) => {
+        state.customerOrderState.loading = true
+      }, false, 'read_customer_order/loading')
+
+      try {
+        const res = await getDataAPI(`/checkout/admin?page=${page}&limit=${limit}&status=${status}`, token)
+
+        set((state: GlobalStoreState) => {
+          state.customerOrderState.data = res.data.checkout
+          state.customerOrderState.totalData = res.data.totalData
+          state.customerOrderState.totalPage = res.data.totalPage
+        }, false, 'read_customer_order/success')
+      } catch (err: any) {
+        set((state: GlobalStoreState) => {
+          state.customerOrderState.data = []
+          state.alertState.message = err.response.data.msg
+          state.alertState.type = 'error'
+        }, false, 'read_customer_order/error')
+      }
+
+      set((state: GlobalStoreState) => {
+        state.customerOrderState.loading = false
+      }, false, 'read_customer_order/done_loading')
+    }
+  }
+}
+
+export default customerOrderStore
