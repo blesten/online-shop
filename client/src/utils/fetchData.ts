cac4c82af6e8aa2df755af8dbf9f567ff5e24f02
Stanley Claudius <stanley.claudius@binus.ac.id>
2024-11-09 13:34:21 +0700

    Fix bug

--- client/src/utils/fetchData.ts
+++ client/src/utils/fetchData.ts
@@ -1,50 +1,55 @@
 import axios from 'axios'
 
 export const getDataAPI = async(url: string, token?: string) => {
-  const res = await axios.get(url, {
+  const res = await axios.get(`http://localhost:5000${url}`, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
 }
 
 export const postDataAPI = async(url: string, data: object, token?: string) => {
-  const res = await axios.post(url, data, {
+  const res = await axios.post(`http://localhost:5000${url}`, data, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
 }
 
 export const patchDataAPI = async(url: string, data: object, token?: string) => {
-  const res = await axios.patch(url, data, {
+  const res = await axios.patch(`http://localhost:5000${url}`, data, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
 }
 
 export const putDataAPI = async(url: string, data: object, token?: string) => {
-  const res = await axios.put(url, data, {
+  const res = await axios.put(`http://localhost:5000${url}`, data, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
 }
 
 export const deleteDataAPI = async(url: string, token?: string) => {
-  const res = await axios.delete(url, {
+  const res = await axios.delete(`http://localhost:5000${url}`, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
